d9b0a66c00a02a3b74713322dcdeea7546d87aa5
Monty Montgomery <xiphmont@gmail.com>
2016-12-30 01:19:12 -0500

    Rebase the Tim+Smarter RD patch for current master

--- av1/encoder/hybrid_fwd_txfm.h
+++ av1/encoder/hybrid_fwd_txfm.h
@@ -13,6 +13,7 @@
 #define AV1_ENCODER_HYBRID_FWD_TXFM_H_
 
 #include "./aom_config.h"
+#include "./aom_dsp_rtcd.h"
 
 typedef enum FWD_TXFM_OPT { FWD_TXFM_OPT_NORMAL } FWD_TXFM_OPT;
 
@@ -36,6 +37,16 @@ void fwd_txfm(const int16_t *src_diff, tran_low_t *coeff, int diff_stride,
 void highbd_fwd_txfm(const int16_t *src_diff, tran_low_t *coeff,
                      int diff_stride, FWD_TXFM_PARAM *fwd_txfm_param);
 #endif  // CONFIG_AOM_HIGHBITDEPTH
+
+static INLINE int get_tx1d_size(TX_SIZE tx_size) {
+  switch (tx_size) {
+    case TX_32X32: return 32;
+    case TX_16X16: return 16;
+    case TX_8X8: return 8;
+    case TX_4X4: return 4;
+    default: assert(0); return -1;
+  }
+}
 
 #ifdef __cplusplus
 }  // extern "C"
