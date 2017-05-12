273b791c541dee31bca058e0e538e13d513e15a2
Monty Montgomery <xiphmont@gmail.com>
2017-05-12 10:57:30 -0400

    Change pure q-mode encoding to not use recode loop

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -4910,7 +4910,7 @@ static void encode_frame_to_data_rate(AV1_COMP *cpi, size_t *size,
   cm->delta_q_present_flag = cpi->oxcf.deltaq_mode != NO_DELTA_Q;
 #endif
 
-  if (cpi->sf.recode_loop == DISALLOW_RECODE) {
+  if (cpi->sf.recode_loop == DISALLOW_RECODE || cpi->oxcf.rc_mode == AOM_Q) {
     encode_without_recode_loop(cpi);
   } else {
     encode_with_recode_loop(cpi, size, dest);
